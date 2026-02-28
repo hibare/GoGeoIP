dc1e2a5874a3a5108e5b40b0482d47d562a7f985
Shubham Hibare <shubham@hibare.in>
2026-02-28 23:23:21 +0530

    chore: update ESLint configuration and dependencies
    - Enhanced ESLint configuration by integrating recommended settings from @eslint/js and typescript-eslint.
    - Updated ESLint rules to disable 'no-console' and 'no-case-declarations'.
    - Added new dependencies: @eslint/js, eslint-plugin-vue, globals, and typescript-eslint.
    - Modified .pre-commit-config.yaml to ensure build command only runs on changes in the 'web/' directory.
    - Updated pnpm-lock.yaml to reflect new package versions and dependencies.

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,11 +1,26 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
 export default [
   {
-    ignores: ['dist/**', 'node_modules/**', 'pnpm-lock.yaml']
+    ignores: ['dist/**', 'node_modules/**', 'pnpm-lock.yaml'],
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
   },
   {
     rules: {
       'no-unused-vars': 'warn',
-      'no-console': 'warn'
-    }
-  }
+      'no-console': 'off',
+      'no-case-declarations': 'off',
+    },
+  },
 ]
