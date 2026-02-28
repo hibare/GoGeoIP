2a497a8e5ca40d2f10dd754be630303c92e625a3
Shubham Hibare <shubham@hibare.in>
2026-02-28 19:37:12 +0530

    fix

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
           cert: fs.readFileSync(certPath),
         },
         proxy: {
-          "^/api": {
+          "^/api/v1": {
             target: "https://localhost:5000",
             changeOrigin: true,
             secure: false,
@@ -39,7 +39,7 @@ export default defineConfig({
     } else {
       return {
         proxy: {
-          "^/api": {
+          "^/api/v1": {
             target: "http://localhost:5000",
             changeOrigin: true,
             secure: false,
